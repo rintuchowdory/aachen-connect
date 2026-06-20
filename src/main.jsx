f7c0382681866efdd4aaa03a80d2e2c17688a1c1
Rintu-chowdory <chowdoryrintu492@gmail.com>
2026-06-20 14:45:53 +0200

    Wrap app with LangProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { LangProvider } from './context/LangContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
-    <App />
+    <LangProvider>
+      <App />
+    </LangProvider>
   </HashRouter>
 )
